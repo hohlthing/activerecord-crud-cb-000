dc2d8e276506d41228ff765c2a1f9fb5adbfa1e3
michael hohl <mhohl@rocketmail.com>
2018-05-04 18:27:26 +0000

    automatically backed up by learn

--- db/migrate/20180504182509_create_movies.rb
+++ db/migrate/20180504182509_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    create_table :movies do |t|
-      t.string :title
-      t.integer :release_date
-      t.string :director
-      t.string :lead
-      t.boolean :in_theaters
+    create_table :movies do |x|
+      x.string :title
+      x.integer :release_date
+      x.string :director
+      x.string :lead
+      x.boolean :in_theaters
    end
   end
 end
