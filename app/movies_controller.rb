426d6f7a7fa3ae065bdbb4dbee514af2adfcfd78
michael hohl <mhohl@rocketmail.com>
2018-05-04 18:43:20 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,10 +22,11 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(args = {})
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    args.each do |k, v|
+      m[k] = v
+    end
   end
 end
 
